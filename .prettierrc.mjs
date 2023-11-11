75777ff42567642cba31655f10f439fab3a61e3e
Michael Lyon <mlyon360@gmail.com>
2023-11-11 15:15:28 -0800

    add prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,11 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+const config = {
+	trailingComma: 'all',
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+}
+
+export default config
