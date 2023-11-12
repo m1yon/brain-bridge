751ac97b570bc35280520df9a0696a545093d2a3
Michael Lyon <mlyon360@gmail.com>
2023-11-12 12:29:11 -0800

    fix formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,22 @@
 /* eslint-env node */
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    'next/core-web-vitals',
-    'plugin:tailwindcss/recommended',
-    'prettier',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: true,
-    tsconfigRootDir: __dirname,
-  },
-  plugins: ['tailwindcss', '@typescript-eslint'],
-  root: true,
-  settings: {
-    tailwindcss: {
-      callees: ['cn'],
-    },
-  },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended-type-checked',
+		'next/core-web-vitals',
+		'plugin:tailwindcss/recommended',
+		'prettier',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: true,
+		tsconfigRootDir: __dirname,
+	},
+	plugins: ['tailwindcss', '@typescript-eslint'],
+	root: true,
+	settings: {
+		tailwindcss: {
+			callees: ['cn'],
+		},
+	},
 }
