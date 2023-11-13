ab5656d20949c60d7a2f27d402245cedd7813b03
Michael Lyon <mlyon360@gmail.com>
2023-11-13 12:30:11 -0800

    use serverEnvVariables rather then process.env directly

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,12 +2,13 @@ import dotenv from 'dotenv'
 import postgres from 'postgres'
 import * as schema from './schema'
 import { drizzle } from 'drizzle-orm/postgres-js'
+import { serverEnvVariables } from '@/env.server'
 
 // required for Drizzle Kit
 dotenv.config()
 dotenv.config({ path: '.env.local', override: true })
 
-const connectionString = process.env.DATABASE_URL!
+const connectionString = serverEnvVariables.DATABASE_URL
 const client = postgres(connectionString, {
 	idle_timeout: 20,
 	max_lifetime: 60,
