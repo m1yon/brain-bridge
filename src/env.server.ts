7f78cc06ed68676af42e3b0bde8f498d177198f7
Michael Lyon <mlyon360@gmail.com>
2023-11-13 12:29:29 -0800

    fix env.server

--- src/env.server.ts
+++ src/env.server.ts
@@ -6,10 +6,7 @@ dotenv.config()
 dotenv.config({ path: '.env.local', override: true })
 
 const serverEnvVariablesSchema = z.object({
-	POSTGRES_HOST: z.string(),
-	POSTGRES_USER: z.string(),
-	POSTGRES_PASSWORD: z.string(),
-	POSTGRES_DATABASE: z.string(),
+	DATABASE_URL: z.string(),
 })
 
 export const serverEnvVariables = serverEnvVariablesSchema.parse(process.env)
