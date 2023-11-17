41e796bb1895f044479a80ccb7a4a0aea048e203
Michael Lyon <mlyon360@gmail.com>
2023-11-16 17:25:00 -0800

    add more back buttons of easier navigation

--- src/app/set/[setId]/update/page.tsx
+++ src/app/set/[setId]/update/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from '@/components/primitives/Button'
 import CreateUpdateSetForm from '@/components/sets/CreateUpdateSetForm'
 import { SetService } from '@/services'
+import { ArrowLeftIcon } from '@radix-ui/react-icons'
 import { unstable_cache } from 'next/cache'
+import Link from 'next/link'
+import { notFound } from 'next/navigation'
 
 export default async function UpdateSetPage({
 	params,
@@ -16,9 +20,19 @@ export default async function UpdateSetPage({
 	)
 	const set = await getSetCached()
 
+	if (!set) {
+		notFound()
+	}
+
 	return (
 		<main className="mx-6 my-12">
-			<h1 className="mb-6 text-4xl font-bold">Update {set?.name}</h1>
+			<Link href={`/set/${set.id}`}>
+				<Button variant="link" className="pl-0">
+					<ArrowLeftIcon className="mr-1 inline" />
+					Back
+				</Button>
+			</Link>
+			<h1 className="mb-6 text-4xl font-bold">Update Set</h1>
 			<CreateUpdateSetForm setToUpdate={set} />
 		</main>
 	)
--- src/app/set/create/page.tsx
+++ src/app/set/create/page.tsx
@@ -1,5 +1,8 @@
+import { Button } from '@/components/primitives/Button'
 import CreateUpdateSetForm from '@/components/sets/CreateUpdateSetForm'
+import { ArrowLeftIcon } from '@radix-ui/react-icons'
 import { Metadata } from 'next'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
 	title: 'Create New Set - Brain Bridge',
@@ -9,6 +12,12 @@ export const metadata: Metadata = {
 export default function CreateSet() {
 	return (
 		<main className="mx-6 my-12">
+			<Link href="/">
+				<Button variant="link" className="pl-0">
+					<ArrowLeftIcon className="mr-1 inline" />
+					Back
+				</Button>
+			</Link>
 			<h1 className="mb-6 text-4xl font-bold">Create New Set</h1>
 			<CreateUpdateSetForm />
 		</main>
