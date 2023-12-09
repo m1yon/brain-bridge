b4b4508fe6d0fd56aea8e6c1b218257baee128c5
Michael Lyon <mlyon360@gmail.com>
2023-12-08 19:19:22 -0800

    fix signIn/signOut issue

--- src/lib/actions/auth-actions.ts
+++ src/lib/actions/auth-actions.ts
@@ -2,20 +2,22 @@
 
 import { AuthService } from '@/lib/services'
 import { z } from 'zod'
-import { action } from './client'
 import { ProviderSchema } from '@/lib/constants/Provider'
 
-const signOutSchema = z.void()
-
-export const signOut = action(signOutSchema, async () => {
+// redirects cannot be called in try/catch blocks (actions wrapper)
+export const signOut = async () => {
 	await AuthService.signOut()
-})
+}
 
 const signInSchema = z.object({
 	provider: ProviderSchema,
 	redirectTo: z.string().optional(),
 })
 
-export const signIn = action(signInSchema, async ({ provider, redirectTo }) => {
+// redirects cannot be called in try/catch blocks (actions wrapper)
+export const signIn = async ({
+	provider,
+	redirectTo,
+}: z.infer<typeof signInSchema>) => {
 	await AuthService.signIn({ provider, redirectTo })
-})
+}
