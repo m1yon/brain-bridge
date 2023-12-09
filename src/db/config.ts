2cdd9b1a73b8cce2539fb6847d7865f562961abc
Michael Lyon <mlyon360@gmail.com>
2023-12-08 17:45:36 -0800

    fix test

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,7 +1,7 @@
-import { normalUserSession } from './seed-data/users'
+import { adminUserSession } from './seed-data/users'
 
 export const config = {
 	seed: {
-		session: normalUserSession,
+		session: adminUserSession,
 	},
 }
