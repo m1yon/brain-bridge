3a27d82d85ba97c97125d3870e76871d30148dfc
Michael Lyon <mlyon360@gmail.com>
2023-12-11 16:27:25 -0800

    add setup script

--- src/scripts/db-migrate.ts
+++ src/scripts/db-migrate.ts
@@ -1,6 +1,9 @@
 import dotenv from 'dotenv'
 import { migrate } from 'drizzle-orm/libsql/migrator'
 import { intro, outro, spinner } from '@clack/prompts'
+import { $ } from 'execa'
+
+await $`mkdir test-dbs`
 
 dotenv.config()
 dotenv.config({ path: '.env.local', override: true })
