86458cb87e7f4359b50a7ffa4094088e18e4455f
Michael Lyon <mlyon360@gmail.com>
2023-12-11 18:29:31 -0800

    add a test for creating a set and immediately viewing the specific set

--- src/lib/data-access/index.test.ts
+++ src/lib/data-access/index.test.ts
@@ -24,6 +24,34 @@ describe('Data-access layer services', () => {
 			expect(resultSet?.userId).toEqual(normalUserSession.user.id)
 			expect(resultSet?.flashcardCount).toEqual(expectedSet.flashcardCount)
 		})
+
+		it('can create a set and immediately view the specific set', async () => {
+			const expectedSet = setBuilder.one()
+
+			const result = await SetService.createSet(expectedSet)
+
+			expect(result).toStrictEqual({
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+				id: expect.stringContaining(''),
+			})
+
+			const resultSet = await SetService.getSet({ id: result.id })
+
+			expect(typeof resultSet?.id).toBe('string')
+			expect(resultSet?.name).toEqual(expectedSet.name)
+			expect(resultSet?.description).toEqual(expectedSet.description)
+			expect(resultSet?.userId).toEqual(normalUserSession.user.id)
+			expect(resultSet?.flashcardCount).toEqual(expectedSet.flashcardCount)
+
+			expectedSet.flashcards.forEach((expectedFlashcard, index) => {
+				const resultFlashcard = resultSet?.flashcards[index]
+
+				expect(typeof resultFlashcard?.id).toBe('string')
+				expect(resultFlashcard?.term).toBe(expectedFlashcard.term)
+				expect(resultFlashcard?.definition).toBe(expectedFlashcard.definition)
+				expect(resultFlashcard?.userId).toBe(normalUserSession.user.id)
+			})
+		})
 	})
 
 	describe('User service', () => {
