a0ac13667791a55b0869353bf4f11b34ecf8f3aa
Michael Lyon <mlyon360@gmail.com>
2023-12-12 14:07:08 -0800

    fix db test isolation

--- src/lib/tests/setup/db-setup.ts
+++ src/lib/tests/setup/db-setup.ts
@@ -1,7 +1,7 @@
 import './mock-setup'
 import path from 'node:path'
 import fsExtra from 'fs-extra'
-import { afterAll, afterEach, beforeAll } from 'vitest'
+import { afterAll, beforeEach } from 'vitest'
 import { BASE_DATABASE_PATH } from './global-setup'
 
 /**
@@ -12,16 +12,8 @@ const databaseFile = `./test-dbs/${process.env.VITEST_POOL_ID || 0}.db`
 const databasePath = path.join(process.cwd(), databaseFile)
 process.env.DATABASE_URL = `file:${databasePath}`
 
-beforeAll(async () => {
+beforeEach(async () => {
 	await fsExtra.copyFile(BASE_DATABASE_PATH, databasePath)
-})
-
-afterEach(async () => {
-	const { db } = await import('@/db')
-	const { sets, users } = await import('@/db/schema')
-
-	await db.delete(users)
-	await db.delete(sets)
 })
 
 afterAll(async () => {
