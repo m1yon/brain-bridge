fe275d33cdfaf81c094954005d4f7016a00a7901
Michael Lyon <mlyon360@gmail.com>
2023-12-12 15:50:35 -0800

    improve data-access file structure

--- src/lib/data-access/index.ts
+++ src/lib/data-access/index.ts
@@ -1,13 +1,2 @@
-import * as drizzleSetOperations from './implementations/DrizzleSet'
-import * as drizzleUserOperations from './implementations/DrizzleUser'
-
-import { SetOperations } from './interfaces/ISet'
-import { UserOperations } from './interfaces/IUser'
-
-export const SetService: SetOperations = {
-	...drizzleSetOperations,
-}
-
-export const UserService: UserOperations = {
-	...drizzleUserOperations,
-}
+export * as SetService from './implementations/SQLiteDrizzleSet'
+export * as UserService from './implementations/SQLiteDrizzleUser'
