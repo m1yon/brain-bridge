2e3387cbc9d1f7fdf70d4267d6771441a3c99ca0
Michael Lyon <mlyon360@gmail.com>
2023-12-11 17:15:44 -0800

    add test DB isolation

---
+++ src/lib/tests/setup/db-setup.ts
@@ -0,0 +1,27 @@
+import path from 'node:path'
+import fsExtra from 'fs-extra'
+import { afterAll, afterEach, beforeAll } from 'vitest'
+import { BASE_DATABASE_PATH } from './global-setup'
+import { db } from '@/db'
+import { sets, users } from '@/db/schema'
+
+/**
+ * This setup file generates individual DBs for each test runner, as well
+ * as ensures test isolation by cleaning up after each test.
+ * */
+const databaseFile = `./test-dbs/${process.env.VITEST_POOL_ID || 0}.db`
+const databasePath = path.join(process.cwd(), databaseFile)
+process.env.DATABASE_URL = `file:${databasePath}`
+
+beforeAll(async () => {
+	await fsExtra.copyFile(BASE_DATABASE_PATH, databasePath)
+})
+
+afterEach(async () => {
+	await db.delete(users)
+	await db.delete(sets)
+})
+
+afterAll(async () => {
+	await fsExtra.remove(databasePath)
+})
--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,9 @@ export default defineConfig({
 	plugins: [tsconfigPaths()],
 	test: {
 		globalSetup: ['./src/lib/tests/setup/global-setup.ts'],
+		setupFiles: [
+			'./src/lib/tests/setup/setup-env.ts',
+			'./src/lib/tests/setup/db-setup.ts',
+		],
 	},
 })
